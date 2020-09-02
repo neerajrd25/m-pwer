d65f147bcf04951836662a7cd58ea2f6285559ee
neerajvkarma <60004467+neerajvkarma@users.noreply.github.com>
2020-09-02 13:50:46 +0530

    Form url encoded

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from 'src/api.service';
 import { of } from 'rxjs';
 import { API_TOKEN } from 'src/app.constants';
+import { Login } from '../_models/login.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,13 @@ export class LoginService {
   constructor(private api: ApiService) { }
 
 
-  doLogin(login){
+  doLogin(login:Login){
     let headers = new Headers();
     // headers.append('Access-Control-Allow-Origin', '*');
     headers.append('apptoken',API_TOKEN);
     headers.append('Content-Type','application/x-form-urlencoded')
-    return this.api.post('login/password',login, headers )
+    let body = `email=${login.email}&password=${login.password}&tenantid=${login.tenantid}&id_type=${login.id_type}`;
+    return this.api.post('login/password',body, headers )
 
 
   }
