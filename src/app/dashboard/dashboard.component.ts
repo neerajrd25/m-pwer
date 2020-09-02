dc6a07c9b777e6255709cc7f8876e6a9f5d06d51
neerajvkarma <60004467+neerajvkarma@users.noreply.github.com>
2020-09-02 13:57:22 +0530

    Form url encoded

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: DashboardService) { }
 
   ngOnInit(): void {
+    this.getTasks()
+  }
+
+  getTasks(){
+    this.service.postTask().subscribe((data)=>{
+      console.log(data);
+    })
   }
 
 }
