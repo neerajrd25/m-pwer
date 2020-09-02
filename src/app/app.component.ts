7669367467f8b638a92b59c029dd3cfcade007c8
neerajvkarma <60004467+neerajvkarma@users.noreply.github.com>
2020-09-02 17:17:36 +0530

    service try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,64 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kognitive';
 
   deferredPrompt: any;
   showButton = false;
-  @HostListener('window:beforeinstallprompt', ['$event'])
-  onbeforeinstallprompt(e) {
-    console.log(e);
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    this.deferredPrompt = e;
-    this.showButton = true;
+
+  constructor(private swUpdate: SwUpdate) {
+
   }
-  addToHomeScreen() {
-    // hide our user interface that shows our A2HS button
-    this.showButton = false;
-    // Show the prompt
-    this.deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    this.deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
+  ngOnInit() {
+
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+        if (confirm("New version available. Load New Version?")) {
+
+          window.location.reload();
         }
-        this.deferredPrompt = null;
       });
+    }
+
+    this.swUpdate.available.subscribe((data)=>{
+        console.log(data);
+    })
+
+    
+
   }
+
+
+  // @HostListener('window:beforeinstallprompt', ['$event'])
+  // onbeforeinstallprompt(e) {
+  //   console.log(e);
+  //   // Prevent Chrome 67 and earlier from automatically showing the prompt
+  //   e.preventDefault();
+  //   // Stash the event so it can be triggered later.
+  //   this.deferredPrompt = e;
+  //   this.showButton = true;
+  // }
+  // addToHomeScreen() {
+  //   // hide our user interface that shows our A2HS button
+  //   this.showButton = false;
+  //   // Show the prompt
+  //   this.deferredPrompt.prompt();
+  //   // Wait for the user to respond to the prompt
+  //   this.deferredPrompt.userChoice
+  //     .then((choiceResult) => {
+  //       if (choiceResult.outcome === 'accepted') {
+  //         console.log('User accepted the A2HS prompt');
+  //       } else {
+  //         console.log('User dismissed the A2HS prompt');
+  //       }
+  //       this.deferredPrompt = null;
+  //     });
+  // }
 }
